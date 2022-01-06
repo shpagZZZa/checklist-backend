60a212d47df4353a3e273202c266b30dba40178c
akuzin <akuzin@tradedealer.ru>
2022-01-06 14:37:56 +0500

    check-user

--- src/Controller/CallController.php
+++ src/Controller/CallController.php
@@ -44,6 +44,29 @@ class CallController extends BaseController
     }
 
     /**
+     * @Route("/check-user/{uniqueId}", name="check-user", methods={"GET"})
+     * @param Request $request
+     * @param string $uniqueId
+     * @return JsonResponse
+     */
+    public function checkUserAction(Request $request, string $uniqueId): JsonResponse
+    {
+        $user = $this->userService->getUser();
+
+        /** @var Checklist $entity */
+        $checklistEntity = $this->em->getRepository(Checklist::class)->findOneBy([
+            'unique_id' => $uniqueId
+        ]);
+        $entity = $this->em->getRepository(Call::class)->findOneBy([
+            'toUser' => $user,
+            'checklist' => $checklistEntity
+        ]);
+        return new JsonResponse([
+            'status' => $entity ? 'created' : 'not_created'
+        ], 200);
+    }
+
+    /**
      * @Route("/{id}", name="get", methods={"GET"})
      * @param Request $request
      * @param int $id
